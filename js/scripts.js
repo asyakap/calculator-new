baaf7a65edf2ac9f3c17894436cc4c5adff96368
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-01-11 11:10:50 -0800

    add voting page and jss

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+
+    let voting = document.getElementById("voting");
+    voting.setAttribute("class", "hidden");
+    let under18Message = document.getElementById("under-18");
+    under18Message.setAttribute("class", "hidden");
+
+    const age = parseInt(document.querySelector("input#age").value);
+
+    if (age > 17) {  // updated to check if age is greater than 17
+      voting.removeAttribute("class");
+    } else {
+      under18Message.removeAttribute("class");
+    }
+  };
+};
